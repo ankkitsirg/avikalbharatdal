360b10c82b4ca109e1032dcc34fda46993650cae
ankkitsirg <ankkitsirg@gmail.com>
2026-03-03 16:19:07 +0530

    modify admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,23 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { useState,useEffect } from "react";
 
 export default async function Admin() {
   const session = await getServerSession(authOptions);
+ const [members, setMembers] = useState([]);
 
+  useEffect(() => {
+    fetch("/api/members")
+      .then((res) => res.json())
+      .then((data) => setMembers(data));
+  }, []);
   if (!session) {
     redirect("/login");
   }
 
   return (
+    <>
     <main className="p-10">
       <h1 className="text-3xl font-bold mb-6">
         Welcome, {session.user?.name}
@@ -17,5 +25,31 @@ export default async function Admin() {
       <p>Role: {session.user?.role}</p>
       <p>Admin dashboard content here.</p>
     </main>
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">Registered Members</h1>
+
+      <table className="w-full border">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="p-2 border">Name</th>
+            <th className="p-2 border">Phone</th>
+            <th className="p-2 border">District</th>
+          </tr>
+        </thead>
+        <tbody>
+          {members.map((m: any, i) => (
+            <tr key={i}>
+              <td className="p-2 border">{m.name}</td>
+              <td className="p-2 border">{m.phone}</td>
+              <td className="p-2 border">{m.district}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+    </>
   );
 }
+
+//"use client";
+
