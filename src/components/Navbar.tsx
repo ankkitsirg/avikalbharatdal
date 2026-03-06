42fc17f904b90741cb1a7899f8e7f208547911b0
ankkitsirg <ankkitsirg@gmail.com>
2026-03-06 19:34:31 +0530

    replace logo and title ico

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold text-orange-600">
-        ABD
-      </h1>
+     <Image
+  src="/logo.png"
+  alt="ABD Logo"
+  width={60}
+  height={60}
+/>
 
       <div className="space-x-6 font-medium">
         <Link href="/" className="hover:text-orange-600">Home</Link>
