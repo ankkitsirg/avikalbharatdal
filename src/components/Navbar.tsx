72faee3271add1a708f245c36917630e4a082746
ankkitsirg <ankkitsirg@gmail.com>
2026-03-06 19:17:01 +0530

    modify navbar fulname to shortname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold text-orange-600">
-        Avikal Bharat Dal
+        ABD
       </h1>
 
       <div className="space-x-6 font-medium">
